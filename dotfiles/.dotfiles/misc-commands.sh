66535da8026fb0dc672ce4ae61378e4bc5b78428
Will Ockmore <will.ockmore@gmail.com>
2025-01-06 10:24:38 +0000

    cursor

--- dotfiles/.dotfiles/misc-commands.sh
+++ dotfiles/.dotfiles/misc-commands.sh
@@ -1,8 +1,19 @@
 # Git
 ,gb() {
-    git checkout $(git branch --sort=-committerdate | fzf)
+    local branch=$(git branch --sort=-committerdate | fzf)
+    if [[ -n "$branch" ]]; then
+        git checkout "$branch"
+    else
+        echo "No branch selected"
+    fi
 }
 
 ,feat() {
-    git checkout main && git pull && git checkout -b "wo/$1"
+    if [[ -z "$1" ]]; then
+        echo "Please provide a feature name"
+        return 1
+    fi
+    git checkout main && \
+    git pull && \
+    git checkout -b "wo/$1"
 }
